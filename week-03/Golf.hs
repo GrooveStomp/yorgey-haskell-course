a3e1b35b86960b7dec62327c4d6c9e7b4ad45de2
GrooveStomp <aaron.oman@gmail.com>
2014-07-09 23:12:53 -0700

    Solved Exercise 1.

--- week-03/Golf.hs
+++ week-03/Golf.hs
@@ -1,14 +1,6 @@
 module Golf where
 
 skips :: [a] -> [[a]]
--- get the size of the list
--- for size to zero:
---   generate a new list that is every nth element of the first list
--- collect all lists into an array
-skips list = 
-
-indices :: Int -> Int -> [Int]
-indices n len = [x*n|x <- [0..len], x*n < len]
-
-fromList :: [a] -> [Int] -> [a]
-fromList = map . (!!)
+skips list = map (\x -> map (list !!) [x-1,x*2-1..listLength-1]) [1..listLength]
+  where
+    listLength = length list
