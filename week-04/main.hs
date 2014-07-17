26aa47fd4beb58ff4fcd99a135cc62b2bad2dddd
GrooveStomp <aaron.oman@gmail.com>
2014-07-16 20:47:02 -0700

    Add second part of Week4 Ex1. Needed help on this, but eventually I understood.

--- week-04/main.hs
+++ week-04/main.hs
@@ -14,14 +14,17 @@ fun1test (x:xs)
 
 -- Pointfree/idiomatic
 fun1 :: [Integer] -> Integer
-fun1 = product . map (\x -> x - 2) . filter even
+fun1 = product . map (+(-2)) . filter even
 
 -- Original implementation.
 fun2test :: Integer -> Integer
 fun2test 1 = 0
-fun2test n | even n = n + fun2test (n `div` 2)
-           | otherwise = fun2test (3 * n + 2)
+fun2test n | even n = n + fun2test (div n 2)
+           | otherwise = fun2test (3 * n + 1)
 
 -- Pointfree/idiomatic
---fun2 :: Integer -> Integer
---fun2 = sum . map (\x -> 
+fun2 :: Integer -> Integer
+fun2 = sum . filter even . takeWhile (>1) . iterate mySeq
+
+mySeq :: Integer -> Integer
+mySeq x = if even x then div x 2 else 3 * x + 1 
