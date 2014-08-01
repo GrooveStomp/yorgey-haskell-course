bd2f572599f948c5115f7e9b25012f0e3ff4910e
GrooveStomp <aaron.oman@gmail.com>
2014-08-01 00:26:37 -0700

    Solution for exercise 2, week 4.

--- week-04/main.hs
+++ week-04/main.hs
@@ -39,26 +39,25 @@ data Tree a = Leaf
             | Node Integer (Tree a) a (Tree a)
   deriving (Show, Eq)
 
---foldTree :: [a] -> Tree a
---foldTree list = foldr myFn Leaf list
--- Trees don't need to be sorted.  Just some binary function.
--- foldr on [a].
--- The function (a -> b -> b) for foldr is key.
--- Our zero is probably a Leaf?
+construct :: a -> Tree a -> Tree a
+construct a Leaf = Node 0 Leaf a Leaf
+construct a (Node _ Leaf        root Leaf        ) = Node 1 (Node 0 Leaf a Leaf) root Leaf
+construct a (Node c left@Node{} root Leaf        ) = Node c left                 root (Node 0 Leaf a Leaf)
+construct a (Node c Leaf        root right@Node{}) = Node c (Node 0 Leaf a Leaf) root right
 
+construct a (Node c left@(Node leftCount ll lt lr) root right@(Node rightCount rl rt rr))
+  | freeSpace left         = Node c     (construct a left) root right
+  | freeSpace right        = Node c     left               root (construct a right)
+  | leftCount < rightCount = Node c     (construct a (Node (leftCount+1) ll lt lr)) root right
+  | leftCount > rightCount = Node c     left               root (construct a (Node (rightCount+1) rl rt rr))
+  | otherwise              = Node (c+1) (construct a left) root right
 
--- The difficult part is starting with the zero at the root and building the
--- tree "downward", then the root node already has two leafs, and these need to
--- be replaced with actual nodes as we expand the foldr outward.
+freeSpace :: Tree a -> Bool
+freeSpace (Node _ (Node _ Leaf _ Leaf) _ (Node _ Leaf _ Leaf)) = False
+freeSpace (Node _ Leaf                 _ Leaf                ) = False
+freeSpace (Node _ Leaf                 _ Node{}              ) = True
+freeSpace (Node _ Node{}               _ Leaf                ) = True
+freeSpace (Node _ left@Node{}          _ right@Node{}        ) = freeSpace left || freeSpace right
 
-
--- Sum powers of two up until the size of the input list. If you sum all
--- consecutive powers up to power N, then this equals to power (N+1)
--- Powers of 2: [1,2,4,8,16..]
--- If length list == 17, then we stop at 16 above. But we don't want 16 because
--- summing the powers of two including 16 is obviously more than 17. But we
--- know that all powers of 2 up until 16 summed together actually equal
--- sixteen, and this is definitely less than 17, so we just take everything but
--- the last element and this is the depth of our tree.
-treeDepth :: [a] -> Int
-treeDepth list = length $ init $ takeWhile (<= length list) $ map (2^) [0..]
+foldTree :: [a] -> Tree a
+foldTree = foldr construct Leaf
